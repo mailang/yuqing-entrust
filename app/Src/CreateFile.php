b3f639ccc7c92661acca7bc287348831fb4aecfb
hunterlr <hongshouzh@163.com>
2018-08-27 16:12:41 +0800

    word

--- app/Src/CreateFile.php
+++ app/Src/CreateFile.php
@@ -273,25 +273,32 @@ class CreateFile{
             $inputFileName = $tmppath.iconv('UTF-8', 'GBK//IGNORE','tempc' ).'.docx';
 
             $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessorRe($inputFileName);
-            $templateProcessor->setValue('time', htmlentities($new->starttime));
-            $templateProcessor->setValue('url', htmlentities($new->link));
-            $templateProcessor->setValue('title', htmlentities($new->title));
+            $templateProcessor->setValue('time', $this->htmlentities($new->starttime));
+            $templateProcessor->setValue('url', $this->htmlentities($new->link));
+            $templateProcessor->setValue('title', $this->htmlentities($new->title));
 
             $contentall = html_entity_decode($new->content);
-            //$contentallst = trim(strip_tags($contentall));
-
 
             $contentall = preg_replace("/<(\/?font.*?)>/si","", $contentall);
             $contentall = preg_replace("/<(\/?span.*?)>/si","", $contentall);
             $contentall = preg_replace("/<(img.*?)>/si","", $contentall);
             $contentalls = preg_replace("/<(\/?p.*?)>/si"," ", $contentall);
             $contentalls = preg_replace("/<(\/?br.*?)>/si"," ", $contentalls);
-            $contentalls = trim($contentalls);
+            $contentalls = preg_replace("/<(\/?.*?)>/si","", $contentalls);
+            $contentalls = str_replace('&nbsp;'," ", $contentalls);
+            $contentalls = preg_replace('/^( |\s|\t)*|( |\s|\t)*$/', '', $contentalls);
 
 
             $contentalls = preg_replace('/[\n\r\t]+/', '', $contentalls);
             $contentalls = str_replace(chr(0xc2).chr(0xa0), ' ', $contentalls);
+
+
+            $contentalls = trim($contentalls);
+
             $contentalls = preg_replace('/\s+/', '{{{explode}}}', $contentalls);
+
+
+            $contentalls = $this->htmlentities($contentalls);
 
             $contents = explode("{{{explode}}}",$contentalls);
 
@@ -299,9 +306,20 @@ class CreateFile{
             foreach ($contents as $content){
                 $templateProcessor->setValue('content', $content,1);
             }
+
             $templateProcessor->saveAs($path.$new->title.'.docx');
 
+
         }
+    }
+
+    function htmlentities($str){
+        $str = str_replace("&","&amp;", $str);
+        $str = str_replace(">","&gt;", $str);
+        $str = str_replace("<","&lt;", $str);
+        $str = str_replace("\"","&quot;", $str);
+        $str = str_replace("'","&apos;", $str);
+        return $str;
     }
 
     function downloadzip($id){
